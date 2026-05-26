8205c764541bd8f0d5b3219a67665e4f5909a18d
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-05-26 11:06:43 -0300

    feat: add home page

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+import { Home } from './src/pages/home';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Status = styled.StatusBar``
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <StatusBar hidden/>
+      <Home/>      
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
