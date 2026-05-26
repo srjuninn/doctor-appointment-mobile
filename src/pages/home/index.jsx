af09d3735bcfcfba99cafcb0682308fd00ba1dff
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-05-26 11:04:34 -0300

    feat: add home greeatings

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,19 @@
+import { Container, Greeatings, GreeatingsAvatar, GreeatingsSpan, GreeatingsText, GreeatingsTitle } from "./style"
+
+export const Home = () => {
+    return(
+            <Container>
+                <Greeatings>
+                    <GreeatingsTitle>
+                        <GreeatingsSpan>
+                            hello
+                        </GreeatingsSpan>
+                        <GreeatingsText>
+                            hi Jarvis
+                        </GreeatingsText>
+                    </GreeatingsTitle>
+                    <GreeatingsAvatar source={require("../../assets/GreeatingsAvatar.png")}></GreeatingsAvatar>
+                </Greeatings>
+            </Container>
+    )
+}
