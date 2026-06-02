af68764de6e3ab68b24e58a924de64040ff4b1dc
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-06-02 09:04:29 -0300

    style: schedule card begin

--- src/components/ScheduleCard/style.js
+++ src/components/ScheduleCard/style.js
@@ -1,6 +1,45 @@
 import styled from "styled-components/native"
 
 export const Container = styled.View`
-    background-color: '#4894FE';
-    width: 327px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-width: 327px;
+    padding: 20px;
+    gap: 16px;
+    background: #4894FE;
+    border-radius: 12px;
+`
+export const ScheduleTop = styled.View`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    align-self: stretch;
+`
+export const ScheduleTopAvatar = styled.View`
+    
+`
+export const ScheduleTopTitle = styled.View`
+    
+`
+export const ScheduleToplabel = styled.View`
+    
+`
+export const ScheduleTopDescription = styled.View`
+    
+`
+export const ScheduleTopIcon = styled.View`
+    
+`
+export const ScheduleBottom = styled.View`
+    
+`
+export const ScheduleBottomContainer = styled.View`
+    
+`
+export const ScheduleBottomIcon = styled.View`
+    
+`
+export const ScheduleBottomTitle = styled.View`
+    
 `
