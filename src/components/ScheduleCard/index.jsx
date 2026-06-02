6eb3bd8d657cfdf53a960dd38b8b01e14656507c
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-06-02 09:45:16 -0300

    feat: add images

--- src/components/ScheduleCard/index.jsx
+++ src/components/ScheduleCard/index.jsx
@@ -1,20 +1,25 @@
-import { Container, ScheduleBottom, ScheduleBottomContainer, ScheduleBottomIcon, ScheduleTop, ScheduleTopAvatar, ScheduleTopDescription, ScheduleTopIcon, ScheduleToplabel, ScheduleTopTitle } from "./style"
+import { Container, ScheduleBottom, ScheduleBottomContainer, ScheduleBottomIcon, ScheduleBottomTitle, ScheduleTop, ScheduleTopContent , ScheduleTopAvatar, ScheduleTopDescription, ScheduleTopIcon, ScheduleToplabel, ScheduleTopTitle } from "./style"
 
 export const SchedyuleCard = () => {
     return(
         <Container>
             <ScheduleTop>
-                <ScheduleTopAvatar/>
-                <ScheduleTopTitle>
-                    <ScheduleToplabel>Dr. Imiram Sayain</ScheduleToplabel>
-                    <ScheduleTopDescription>General Doctor</ScheduleTopDescription>
-                </ScheduleTopTitle>
-                <ScheduleTopIcon/>
+                <ScheduleTopContent>
+                    <ScheduleTopAvatar source={require("../../assets/doctorAvatarImage.png")}/>
+                    <ScheduleTopTitle>
+                        <ScheduleToplabel>Dr. Imiram Sayain</ScheduleToplabel>
+                        <ScheduleTopDescription>General Doctor</ScheduleTopDescription>
+                    </ScheduleTopTitle>
+                </ScheduleTopContent>
+                <ScheduleTopIcon source={require("../../assets/arrowRight.png")}/>
             </ScheduleTop>
             <ScheduleBottom>
                 <ScheduleBottomContainer>
-                    <ScheduleBottomIcon/>
-                    <SheduleBottomTitle></SheduleBottomTitle>
+                    <ScheduleBottomIcon source={require("../../assets/calendar.png")}/>
+                    <ScheduleBottomTitle></ScheduleBottomTitle>
+                </ScheduleBottomContainer><ScheduleBottomContainer>
+                    <ScheduleBottomIcon source={require("../../assets/clock.png")}/>
+                    <ScheduleBottomTitle></ScheduleBottomTitle>
                 </ScheduleBottomContainer>
             </ScheduleBottom>
         </Container>
